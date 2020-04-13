e8baf548f7e29229c0c2c52b6720c08b4c4c01f5
Linh Duong <dnl2612@live.missouristate.edu>
2020-04-13 10:13:49 +0700

    Linked the pesticide database

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'IDS_DB.db'),
-
+    }
+    'pesticides': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'PEST_DB.db'),
     }
 }
 
